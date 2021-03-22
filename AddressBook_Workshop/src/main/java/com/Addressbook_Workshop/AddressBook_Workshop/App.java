db485dc8ed47f9220a66e4777071fc833557865c
“siddiq76” <siddiq.khan0786@gmail.com>
2021-03-22 08:40:38 +0530

    add

---
+++ AddressBook_Workshop/src/main/java/com/Addressbook_Workshop/AddressBook_Workshop/App.java
@@ -0,0 +1,13 @@
+package com.Addressbook_Workshop.AddressBook_Workshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
